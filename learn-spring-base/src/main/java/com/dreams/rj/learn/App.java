2821843127157ad4ba17eee6d52b8c9d6ba9a91d
dreamsrj <dreams.rj@aliyun.com>
2020-06-17 22:54:43 +0800

    add spring annotation

---
+++ learn-spring-base/src/main/java/com/dreams/rj/learn/App.java
@@ -0,0 +1,15 @@
+package com.dreams.rj.learn;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
